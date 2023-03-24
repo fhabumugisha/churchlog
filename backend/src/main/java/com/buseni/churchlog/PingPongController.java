690ce79e74f2fd6febd235c170cf803060c77fc6
fhabumugisha <fabriceboris@gmail.com>
2023-03-24 18:19:50 +0100

    add CD workflow

---
+++ backend/src/main/java/com/buseni/churchlog/PingPongController.java
@@ -0,0 +1,15 @@
+package com.buseni.churchlog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+}
