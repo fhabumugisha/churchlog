e360a63eb1ee510ff1e3ad3ff8283973023c6b51
Habumugisha, Fabrice (ITDV TA) - AF (ext) <fahabumugisha-ext@airfrance.fr>
2023-05-05 12:18:55 +0200

    init angular client

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import  { InputTextModule} from 'primeng/inputtext';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
